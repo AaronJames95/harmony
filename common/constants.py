5c3ce71bc47b77d739307824a4c45f0565714e48
Aaron James <aa.collins.95@gmail.com>
2026-01-02 13:23:44 -0500

    Updated constants to be relevant to project

--- common/constants.py
+++ common/constants.py
@@ -1,1 +1,5 @@
-pi = 3.14
+# common/constants.py
+SERVER_IP = "100.x.x.x"  # Replace with your Workstation's Tailscale IP
+PORT = 8000
+API_URL = f"http://{SERVER_IP}:{PORT}/transcribe"
+LARGE_FILE_THRESHOLD_MB = 100
